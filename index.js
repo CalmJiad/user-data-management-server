45c340d565fbb564490766e5c2156d86102114d4
calmjiad <unlucky4246@gmail.com>
2021-11-30 02:14:29 +0600

    finalized

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qmv2p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("userRegisterData");
+    const usersCollection = database.collection("users");
+
+    // POST api to post users data
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    //     Get api to get users data
+    app.get("/users", async (req, res) => {
+      const cursor = usersCollection.find({});
+      const users = await cursor.toArray();
+      res.send(users);
+    });
+  } finally {
+    // await client.close
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
